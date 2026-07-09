0f4b6a83b104e4fab2a4ed285e71767ff0eee062
CEODE <mungn0603@gmail.com>
2026-07-09 23:52:37 +0900

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-"""app.py - 호환용 엔트리. 실제 메인은 chemical_reactor_app.py 이다.
-
-레포별 <주제>_app.py 규칙을 따른다. 이 파일은 지워도 되며,
-GitHub 웹에서 휴지통 아이콘으로 삭제해도 된다.
-"""
-from chemical_reactor_app import main
-
-if __name__ == "__main__":
-    main()
